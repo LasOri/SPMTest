4fbd2a1bcf2205dfd9746fc0b3d2565ba41533db
LasOri <laszlo.ori@emarsys.com>
2020-01-30 16:17:55 +0100

    .

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "EmarsysSDK",
-            targets: ["EmarsysSDK"]),
+            targets: ["EmarsysSDK", "NotificationService"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
